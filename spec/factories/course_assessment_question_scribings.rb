4fc3e4714b4da1b0f6a20493e4713354076de045
Nicholette <nekonekonik@gmail.com>
2017-02-27 12:35:33 +0800

    Scribing question creation

---
+++ spec/factories/course_assessment_question_scribings.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :course_assessment_question_scribings,
+          class: Course::Assessment::Question::Scribing,
+          parent: :course_assessment_question do
+    attempt_limit 3
+
+  end
+end
