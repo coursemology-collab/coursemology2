6148b7a00d0fdd5c0f10016cdeead8a2ac8ff99d
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-04-21 14:58:45 +0800

    Create empty submission edit page in react

---
+++ client/app/bundles/course/assessment/submission/submission.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import ProviderWrapper from 'lib/components/ProviderWrapper';
+import storeCreator from './store';
+import routes from './routes';
+
+$(document).ready(() => {
+  const mountNode = document.getElementById('assessment-edit');
+
+  if (mountNode) {
+    const store = storeCreator({ submission: {} });
+
+    render(
+      <ProviderWrapper {...{ store }}>
+        <Router routes={routes} history={browserHistory} />
+      </ProviderWrapper>
+    , mountNode);
+  }
+});
