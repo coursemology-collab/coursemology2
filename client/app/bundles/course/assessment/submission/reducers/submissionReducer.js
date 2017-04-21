d053c3a492734775b46ea2e4b0e49cabbf2629c0
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-04-21 14:58:45 +0800

    Create empty submission edit page in react

---
+++ client/app/bundles/course/assessment/submission/reducers/submissionReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  submissionType: null,
+};
+
+export default function submissionReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
