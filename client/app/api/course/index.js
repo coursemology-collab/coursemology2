2a794ab95014fcbc4d39be1afa7d82585cceebc6
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-04-21 17:37:05 +0800

    Create React Submission API

--- client/app/api/course/index.js
+++ client/app/api/course/index.js
@@ -1,11 +1,11 @@
-import AssessmentsAPI from './Assessments';
+import AssessmentAPI from './Assessment';
 import VirtualClassroomsAPI from './VirtualClassrooms';
 import MaterialsAPI from './Materials';
 import MaterialFoldersAPI from './MaterialFolders';
 import SurveyAPI from './Survey';
 
 const CourseAPI = {
-  assessments: new AssessmentsAPI(),
+  assessment: AssessmentAPI,
   virtualClassrooms: new VirtualClassroomsAPI(),
   materials: new MaterialsAPI(),
   materialFolders: new MaterialFoldersAPI(),
