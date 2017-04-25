ce9bfb573a7daba434f317b689dd34d6ed4c5acd
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-04-25 16:18:53 +0800

    Render questions based on question type

--- client/app/bundles/course/assessment/submission/constants.js
+++ client/app/bundles/course/assessment/submission/constants.js
@@ -7,6 +7,14 @@ export const DATA_STATES = {
   Received: 'received',
 };
 
+export const QuestionTypes = {
+  MultipleChoice: 'MultipleChoice',
+  MultipleResponse: 'MultipleResponse',
+  Programming: 'Programming',
+  TextResponse: 'TextResponse',
+  FileUpload: 'FileUpload',
+};
+
 const actions = mirrorCreator([
   'FETCHING_SUBMISSION',
   'RECEIVED_SUBMISSION',
