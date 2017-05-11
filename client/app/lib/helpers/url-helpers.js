d454dd8cf2c81ba74870676a9be78afea31507fb
Jeremy Yap <jeremy.yapjl@gmail.com>
2017-05-11 17:54:58 +0800

    Implement answer autograding

--- client/app/lib/helpers/url-helpers.js
+++ client/app/lib/helpers/url-helpers.js
@@ -52,5 +52,6 @@ function getAssessmentId() {
   return match && match[1];
 }
 
+
 /* eslint-disable import/prefer-default-export */
 export { getUrlParameter, getCourseId, getSurveyId, getAssessmentId };
