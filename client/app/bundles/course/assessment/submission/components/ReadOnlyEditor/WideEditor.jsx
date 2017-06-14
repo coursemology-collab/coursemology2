53194652438b581e6b1698fd64da93f01920b477
Jonathan Chiam <jczjchiam@gmail.com>
2017-06-14 17:27:03 +0800

    Implement wide editor

--- client/app/bundles/course/assessment/submission/components/ReadOnlyEditor/WideEditor.jsx
+++ client/app/bundles/course/assessment/submission/components/ReadOnlyEditor/WideEditor.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
-import { Overlay } from 'react-overlays';
-import { grey200, blue500 } from 'material-ui/styles/colors';
+import { grey200 } from 'material-ui/styles/colors';
 
+import WideComments from './WideComments';
 import AddCommentIcon from './AddCommentIcon';
-import OverlayTooltip from './OverlayTooltip';
-import Annotations from '../../containers/Annotations';
 import { AnnotationProp } from '../../propTypes';
 
 const styles = {
@@ -20,7 +17,6 @@ const styles = {
     borderColor: grey200,
     borderRadius: 5,
     overflow: 'auto',
-    width: '50%',
   },
   editor: {
     borderStyle: 'solid',
@@ -44,104 +40,77 @@ const styles = {
     borderRightColor: grey200,
     padding: '0 5px',
   },
-  commentIcon: {
-    color: blue500,
-  },
-  chevronIcon: {
-    fontSize: 10,
-  },
-  chevronIconCollapsed: {
-    fontSize: 10,
-    transform: 'rotate(-90deg)',
-  },
 };
 
 export default class WideEditor extends Component {
-  renderCommentIcon(lineNumber) {
-    const { expanded, annotations, toggleLine } = this.props;
-
-    const annotation = annotations.find(a => a.line === lineNumber);
-    const shouldShow = annotation || expanded[lineNumber - 1];
-
-    return (
-      <div
-        ref={(c) => { this[`comment-${lineNumber}`] = c; }}
-        onClick={() => toggleLine(lineNumber)}
-        style={{ display: 'flex', visibility: shouldShow ? 'visible' : 'hidden', zIndex: 1000 }}
-      >
-        <i className="fa fa-comment" style={styles.commentIcon} />
-        <i
-          className="fa fa-chevron-down"
-          style={expanded[lineNumber - 1] ? styles.chevronIcon : styles.chevronIconCollapsed}
-        />
-      </div>
-    );
-  }
-
-  renderComments(lineNumber) {
-    const { answerId, fileId, annotations, expanded, collapseLine } = this.props;
-    const annotation = annotations.find(a => a.line === lineNumber);
-    const placement = 'left';
-
-    return (
-      <Overlay
-        show={expanded[lineNumber - 1]}
-        onHide={() => collapseLine(lineNumber)}
-        placement={placement}
-        target={() => findDOMNode(this[`comment-${lineNumber}`])}
-      >
-        <OverlayTooltip placement={placement}>
-          <Annotations answerId={answerId} fileId={fileId} lineNumber={lineNumber} annotation={annotation} />
-        </OverlayTooltip>
-      </Overlay>
-    );
-  }
-
   renderLineNumberColumn(lineNumber) {
     const { expandLine } = this.props;
     return (
       <div style={styles.editorLineNumber}>
-        <div>
-          {this.renderCommentIcon(lineNumber)}
-          {this.renderComments(lineNumber)}
-        </div>
         {lineNumber}
         <AddCommentIcon onClick={() => expandLine(lineNumber)} />
       </div>
     );
   }
 
-  render() {
+  renderComments() {
+    const { answerId, fileId, expanded, annotations, expandLine, collapseLine } = this.props;
+    return (
+      <WideComments
+        answerId={answerId}
+        fileId={fileId}
+        annotations={annotations}
+        expanded={expanded}
+        expandLine={lineNumber => expandLine(lineNumber)}
+        collapseLine={lineNumber => collapseLine(lineNumber)}
+      />
+    );
+  }
+
+  renderEditor() {
     /* eslint-disable react/no-array-index-key */
     const { content } = this.props;
     return (
-      <div style={styles.layout}>
-        <div style={styles.editorContainer}>
-          <table style={styles.editor}>
-            <tbody>
-              <tr>
-                <td style={{ width: 75 }}>
-                  {content.map((line, index) =>
-                    <div key={`${index}-${line}`}>
-                      {this.renderLineNumberColumn(index + 1)}
-                    </div>
-                  )}
-                </td>
-                <td>
-                  {content.map((line, index) => {
-                    if (line.trim().length === 0) {
-                      return <div key={`${index}-break`}><br /></div>;
-                    }
-                    return <div key={`${index}-${line}`} style={styles.editorLine}>{line}</div>;
-                  })}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div style={styles.editorContainer}>
+        <table style={styles.editor}>
+          <tbody>
+            <tr>
+              <td style={{ width: 75 }}>
+                {content.map((line, index) =>
+                  <div key={`${index}-${line}`}>
+                    {this.renderLineNumberColumn(index + 1)}
+                  </div>
+                )}
+              </td>
+              <td>
+                {content.map((line, index) => {
+                  if (line.trim().length === 0) {
+                    return <div key={`${index}-break`}><br /></div>;
+                  }
+                  return <div key={`${index}-${line}`} style={styles.editorLine}>{line}</div>;
+                })}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
     /* eslint-enable react/no-array-index-key */
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td style={{ width: '50%' }}>{this.renderComments()}</td>
+              <td style={{ width: '50%' }}>{this.renderEditor()}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
@@ -153,11 +122,9 @@ WideEditor.propTypes = {
   content: PropTypes.arrayOf(PropTypes.string).isRequired,
   expandLine: PropTypes.func,
   collapseLine: PropTypes.func,
-  toggleLine: PropTypes.func,
 };
 
 WideEditor.defaultProps = {
   expandLine: () => {},
   collapseLine: () => {},
-  toggleLine: () => {},
 };
